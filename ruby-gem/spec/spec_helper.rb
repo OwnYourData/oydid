213242434d56c9d4a9e34aab9c342c893bdf6d05
Christoph Fabianek <christoph.fabianek@gmail.com>
2022-02-07 14:26:30 +0100

    coveralls files

--- ruby-gem/spec/spec_helper.rb
+++ ruby-gem/spec/spec_helper.rb
@@ -12,11 +12,11 @@ begin
     config.report_with_single_file = true
     config.single_report_path = 'coverage/lcov.info'
   end
-
   SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
     SimpleCov::Formatter::HTMLFormatter,
     SimpleCov::Formatter::LcovFormatter
   ])
+  
   SimpleCov.start do
     add_filter "/spec/"
   end
